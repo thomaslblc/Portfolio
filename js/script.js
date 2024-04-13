653b38cf32278fa3c67d6afe77cc7c3d136161f5
Thomas <thomaslblc@gmail.com>
2024-04-13 16:22:46 +0200

    FIX popup + STYLE popup

--- js/script.js
+++ js/script.js
@@ -28,36 +28,19 @@ links.forEach(link => {
 
 //POPUP
 
-// Sélectionnez toutes les divs avec la classe "projet"
 const projets = document.querySelectorAll('.projet');
 
-// Pour chaque div "projet", ajoutez un écouteur d'événements au clic
 projets.forEach(projet => {
     projet.addEventListener('click', () => {
-        // Obtenez la valeur de l'attribut de données "data-popup-id"
         const popupId = projet.dataset.popupId;
-        // Sélectionnez la div de la popup correspondante
         const popup = document.getElementById(popupId);
-        // Ajoutez la classe "active" à la popup
         popup.classList.add('active');
     });
 });
 
-// Sélectionnez les éléments popupCover et popupClose
-const popupCover = document.querySelector('.popupCover');
-const popupClose = document.querySelector('.popupClose');
-
-// Ajoutez des écouteurs d'événements pour le clic sur popupCover et popupClose
-popupCover.addEventListener('click', () => {
-    // Sélectionnez la div popupContainer parente
-    const popupContainer = popupCover.closest('.popupContainer');
-    // Retirez la classe "active" de la div popupContainer parente
-    popupContainer.classList.remove('active');
-});
-
-popupClose.addEventListener('click', () => {
-    // Sélectionnez la div popupContainer parente
-    const popupContainer = popupClose.closest('.popupContainer');
-    // Retirez la classe "active" de la div popupContainer parente
-    popupContainer.classList.remove('active');
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('popupCover') || event.target.classList.contains('popupClose')) {
+        const popupContainer = event.target.closest('.popupContainer');
+        popupContainer.classList.remove('active');
+    }
 });
